3380ccf275cdb040be0c29665aecb40ad38ef90d
Robert Jansen <robjansen15@outlook.com>
2017-02-13 12:55:04 -0500

    updated into one webapp

--- ServiceRunner/Program.cs
+++ ServiceRunner/Program.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ServiceApp;
 
 namespace ServiceRunner
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //This starts the process that the service would typically call
-            //This is for debugging purposes
-            new Runner().Run();
+        { 
 
+            Runner r = new Runner();
+
+            r.Run();
+
+            //so you can view output
             Console.Read();
         }
     }
---
+++ ServiceRunner/Runner.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceRunner
+{
+    class Runner
+    {
+        public Runner()
+        {
+            //this is where you can initialize variables
+        }
+
+
+        //do your stuff here
+        public void Run()
+        {
+            
+        }
+    }
+}
