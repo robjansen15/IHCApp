d09532ad0e8015135458c29bcc63c9ef42cc2eee
Robert Jansen <robjansen15@outlook.com>
2017-02-13 14:50:05 -0500

    save

--- IHCWebApp/FamilyForm.aspx
+++ IHCWebApp/FamilyForm.aspx
@@ -9,6 +9,70 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            
+            <asp:Panel runat="server" id="familyPanel">
+                
+                <%--family name--%>
+                <asp:TextBox runat="server" id="familyName"></asp:TextBox>
+            
+                
+                <%-- Family Name
+                
+                Family members
+                    -fname
+                    -lname
+                    -dob
+                    -gender
+                    -is host?--%>
+                
+            </asp:Panel>
+            
+              
+            
+            <asp:Panel runat="server" id="contactInfoPanel" Enabled="False">
+                 <%-- address 
+                
+                primary phone
+                secondary phone
+                
+                email address--%>       
+                
+                <asp:TextBox runat="server">contactInfo</asp:TextBox>    
+
+            </asp:Panel>
+            
+            
+            <asp:Panel runat="server" id="livingDetailsPanel" Enabled="False">
+                <%--smoking policy - y / n
+                host smoke - y / n
+                drinking policy - y / n
+                host drink - y / n
+
+                pets (type, size & quanitty)
+                
+                student bathrooms - # bathrooms student has access to shared?
+                
+                travel to school - length, bus number, more details--%>
+                
+                <asp:TextBox runat="server">living details</asp:TextBox>    
+            </asp:Panel>
+            
+            
+            
+            <asp:Panel runat="server" ID="moreInfoPanel" Enabled="False">
+                
+                <asp:TextBox runat="server">more info</asp:TextBox>    
+                <%--Hobbies
+               
+
+                about--%>
+            </asp:Panel>
+            
+            
+            
+            <asp:Button runat="server" id="backButon" OnClick="backButon_OnClick" Text="Go Back"/>
+            <asp:Button runat="server" id="continueButton" OnClick="continueButton_OnClick" Text="Continue"/>
+
         </div>
     </form>
 </body>
--- IHCWebApp/FamilyForm.aspx.cs
+++ IHCWebApp/FamilyForm.aspx.cs
@@ -2,16 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.SessionState;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+
 
 namespace UserWebApp
 {
     public partial class FamilyForm : System.Web.UI.Page
     {
+
+        
         protected void Page_Load(object sender, EventArgs e)
         {
-
+           
         }
+
+
+        protected void backButon_OnClick(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(Session["panelCounter"].ToString()) > 0)
+            {
+                Session["panelCounter"] = Convert.ToInt32(Session["panelCounter"].ToString()) - 1;
+            }
+
+            DisplayPanel(Convert.ToInt32(Session["panelCounter"].ToString()));
+        }
+
+
+        protected void continueButton_OnClick(object sender, EventArgs e)
+        {
+            if (Session["panelCounter"] == null)
+            {
+                Session["panelCounter"] = 0;
+            }
+
+            DisplayPanel(Convert.ToInt32(Session["panelCounter"].ToString()));
+        }
+
+        public void DisplayPanel(int value)
+        {
+            if (value == 0)
+            {
+                familyPanel.Enabled = true;
+                contactInfoPanel.Enabled = false;
+                livingDetailsPanel.Enabled = false;
+                moreInfoPanel.Enabled = false;
+            }
+            else if (value == 1)
+            {
+                familyPanel.Enabled = false;
+                contactInfoPanel.Enabled = true;
+                livingDetailsPanel.Enabled = false;
+                moreInfoPanel.Enabled = false;
+            }
+            else if (value == 2)
+            {
+                familyPanel.Enabled = false;
+                contactInfoPanel.Enabled = false;
+                livingDetailsPanel.Enabled = true;
+                moreInfoPanel.Enabled = false;
+            }
+            else if (value == 3)
+            {
+                familyPanel.Enabled = false;
+                contactInfoPanel.Enabled = false;
+                livingDetailsPanel.Enabled = false;
+                moreInfoPanel.Enabled = true;
+            }
+        }
+
+        //string firstName = "Jeff";
+        //string lastName = "Smith";
+        //string city = "Seattle";
+
+        //// Save to session state in a Web Forms page class.
+        //Session["FirstName"] = firstName;
+        //Session["LastName"] = lastName;
+        //Session["City"] = city;
+
+        //// Read from session state in a Web Forms page class.
+        //firstName = (string) (Session["FirstName"]);
+        //lastName = (string) (Session["LastName"]);
+        //city = (string) (Session["City"]);
+
+        //// Outside of Web Forms page class, use HttpContext.Current.
+        //HttpContext context = HttpContext.Current;
+        //        context.Session["FirstName"] = firstName;
+        //firstName = (string) (context.Session["FirstName"]);
     }
 }
