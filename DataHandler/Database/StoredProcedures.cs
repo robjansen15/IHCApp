ac31f2d397cd54426a2100ae9ecfc4052caa4529
Rob <robjansen15@gmail.com>
2017-03-07 20:45:48 -0500

    Revert "Revert "Unfinished/tested""
    This reverts commit cb143d73b98ab1bc53a0cb0397c8a26e8ad4cee3.

---
+++ DataHandler/Database/StoredProcedures.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataHandler.Database
+{
+    public class DatabaseHelper
+    {
+        public DatabaseHelper(){ }
+
+
+        /*
+        Purpose: Sample read stored procedure
+        Last Date Modified: 1/25
+        Tested: True
+        Explanation: 
+        */
+        public void ReadStoredProcedure(DatabaseConnection dbConnection)
+        {
+            dbConnection.Connect();
+
+            try
+            {
+                SqlDataReader reader = null;
+                SqlCommand command = new SqlCommand("procedure here", dbConnection._Connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    /*Do what you need to do here for reading*/
+                }
+            }
+            catch (Exception e)
+            {
+                //Variables.GetInstance.Log.Print(e.ToString());
+            }
+
+            dbConnection.Disconnect();
+        }
+        public void ReadStudentBySchool(DatabaseConnection dbConnection, string schoolName)
+        {
+            dbConnection.Connect();
+
+            try
+            {
+                SqlDataReader reader = null;
+                SqlCommand command = new SqlCommand("SPReadStudentBySchool", dbConnection._Connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    
+                }
+            }
+            catch (Exception e)
+            {
+                //Variables.GetInstance.Log.Print(e.ToString());
+            }
+
+            dbConnection.Disconnect();
+        }
+
+        /*
+        Purpose: Sample write stored procedure
+        Last Date Modified: 1/25
+        Tested: False
+        Explanation: 
+        */
+        public void WriteStoredProcedure(DatabaseConnection dbConnection, string value, string value1)
+        {
+            dbConnection.Connect();
+
+            try
+            {
+                SqlCommand command = new SqlCommand("procedure here", dbConnection.Connection);
+                command.CommandType = CommandType.StoredProcedure;
+
+                //Add as many parameters as you want
+                command.Parameters.Add(new SqlParameter("@param", value));
+                command.Parameters.Add(new SqlParameter("@param1", value1));
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Variables.GetInstance.Log.Print(e.ToString());
+            }
+
+            dbConnection.Disconnect();
+        }
+
+    }
+}
--- DataHandler/Models/Family.cs
+++ DataHandler/Models/Family.cs
@@ -7,5 +7,55 @@ namespace DataHandler.Models
 {
     public class Family
     {
+        public Family(string timeToCenter, int numBathrooms, string dogsYN, string catsYN, int numDogs,
+            int numCats, int numRooms, string about, string email, string street, string state, string city,
+            string country, string zip, string primePhone, string secPhone, string hobbies, string looking, string occupied,
+            string note, string toadmin)
+        {
+            _TimeToCenter = timeToCenter;
+            _NumBathrooms = numBathrooms;
+            _DogsYN = dogsYN;
+            _CatsYN = catsYN;
+            _NumDogs = numDogs;
+            _NumCats = numCats;
+            _NumRooms = numRooms;
+            _About = about;
+            _Email = email;
+            _Street = street;
+            _State = state;
+            _City = city;
+            _Country = country;
+            _Zip = zip;
+            _PrimePhone = primePhone;
+            _SecPhone = secPhone;
+            _Hobbies = hobbies;
+            _Looking = looking;
+            _Occupied = occupied;
+            _Note = note;
+            _ToAdmin = toadmin;
+         
+        }
+
+        public string _TimeToCenter { get; set; }
+        public int _NumBathrooms { get; set; }
+        public string _DogsYN { get; set; }
+        public string _CatsYN { get; set; }
+        public int _NumDogs { get; set; }
+        public int _NumCats { get; set; }
+        public int _NumRooms { get; set; }
+        public string _About { get; set; }
+        public string _Email { get; set; }
+        public string _Street { get; set; }
+        public string _State { get; set; }
+        public string _City { get; set; }
+        public string _Country { get; set; }
+        public string _Zip { get; set; }
+        public string _PrimePhone { get; set; }
+        public string _SecPhone { get; set; }
+        public string _Hobbies { get; set; }
+        public string _Looking { get; set; }
+        public string _Occupied { get; set; }
+        public string _Note { get; set; }
+        public string _ToAdmin { get; set; }
     }
 }
