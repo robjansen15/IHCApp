f52da63242e3a3c8243be6da57a206be9e26f662
Rob <robjansen15@gmail.com>
2017-03-10 00:06:58 -0500

    close, almost there...

---
+++ IHCWebApp.Tests/StudentFormTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IHCWebApp;
+
+namespace IHCWebApp.Tests
+{
+    [TestClass]
+    public class StudentFormTests
+    {
+        [TestMethod]
+        public void InsertStudentTest()
+        {
+         
+        }
+    }
+}
