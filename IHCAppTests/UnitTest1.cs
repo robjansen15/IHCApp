06b2b7f015443426249a4a2b7eaf03895eb33786
Robert Jansen <robjansen15@outlook.com>
2017-03-28 23:42:17 -0400

    added test cases

---
+++ IHCAppTests/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IHCApp.Database;
+using IHCApp.Models;
+using IHCApp.Helper;
+
+namespace IHCAppTests
+{
+    [TestClass]
+    public class InsertionTests
+    {
+        [TestMethod]
+        public void ApplicantInsertTest()
+        {
+            try
+            {
+                Applicant applicant = new Applicant();
+
+                new DatabaseConnection()._PublicStrategy._InsertStrategy.InsertApplicant(applicant);
+            }
+            catch(Exception e)
+            {
+                var ex = e.ToString();
+                throw new Exception("Test #1 failed" + ex);
+            }
+           
+        }
+    }
+}
