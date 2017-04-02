0cbd5df05a8cfe66434cbb20c44c186d97e1d18a
Robert Jansen <robjansen15@outlook.com>
2017-04-01 20:05:36 -0400

    added test cases

---
+++ IHCAppTests/Insertion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IHCApp.Database;
+using IHCApp.Models;
+using IHCApp.Helper;
+using System.Collections.Generic;
+
+namespace IHCAppTests
+{
+    [TestClass]
+    public class InsertionTests
+    {
+        [TestMethod]
+        public void ApplicantInsertTest()
+        {
+            try
+            {
+                Applicant applicant = new Applicant();
+
+                new DatabaseConnection()._PublicStrategy._InsertStrategy.InsertApplicant(applicant);
+            }
+            catch(Exception e)
+            {
+                var ex = e.ToString();
+                throw new Exception("Failed to insert Applicant" + ex);
+            }
+           
+        }
+
+
+
+        [TestMethod]
+        public void HostInsertTest()
+        {
+
+            try
+            {
+                Host host = new Host(-1, 15, 2, "yes", "no", 1, 2, 3, "about", "email", "street address", "IN", "Greenwood", "USA", "46142", "phone", "secPhone", "hoobies", "islooking", "occupied", "notes", "toadmin");
+
+                string uid = new DatabaseConnection()._PublicStrategy._InsertStrategy.InsertHost(host);
+
+
+                List<FamilyMember> familyMembers = new List<FamilyMember>();
+
+                familyMembers.Add(new FamilyMember());
+
+               
+            }
+            catch (Exception e)
+            {
+                var ex = e.ToString();
+                throw new Exception("Failed to Insert Host" + ex);
+            }
+
+        }
+    }
+}
---
+++ IHCAppTests/Retrieval.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IHCAppTests
+{
+    [TestClass]
+    public class Retrieval
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
