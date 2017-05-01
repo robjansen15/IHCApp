20da8fed53adc9dc5402e733766bd7cc45041ac8
Robert Jansen <robjansen15@outlook.com>
2017-05-01 08:15:26 -0400

    save

---
+++ SQLQuery3.sql
@@ -0,0 +1,13 @@
+﻿ALTER PROCEDURE [dbo].[ForgotPassword]
+	@USERNAME varchar(25)
+AS
+	select Admin_Password, Admin_Email from Admin 
+	where Admin_Username = @USERNAME
+RETURN 0
+
+
+select Admin_Password, Admin_Email from Admin 
+where Admin_Username = 'Xiao'
+
+
+
